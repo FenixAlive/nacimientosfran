66685a5368e4d1eebf7142a7bf450d63db900920
FenixAlive <luisangelmf@gmail.com>
2017-06-10 17:48:31 -0500

    agregue vistas al node falta hacer las views

--- js/node.js
+++ js/node.js
@@ -7,4 +7,44 @@ app.get("/", function(req,res){
 	res.render("../index")
 });
 
+
+app.get("/chiquito", function(req,res){
+	res.render("chiquito")
+});
+
+
+app.get("/mini", function(req,res){
+	res.render("mini")
+});
+
+
+app.get("/cisco", function(req,res){
+	res.render("cisco")
+});
+
+
+app.get("/mini-especial", function(req,res){
+	res.render("mini-especial")
+});
+
+
+app.get("/francis", function(req,res){
+	res.render("francis")
+});
+
+
+app.get("/estrella", function(req,res){
+	res.render("estrella")
+});
+
+
+app.get("/casita", function(req,res){
+	res.render("casita")
+});
+
+
+app.get("/templo", function(req,res){
+	res.render("templo")
+});
+
 app.listen(8888);
